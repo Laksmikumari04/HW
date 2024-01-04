e57171565522c2172b40cfcbc1b1a2e98705f8bb
Lakshmi <meetlaki11@gmail.com>
2024-01-04 23:34:52 +0530

    This is new file added

--- src/Dec30/Lab101.java
+++ src/Dec30/Lab101.java
@@ -4,5 +4,6 @@ public class Lab101 {
     public static void main(String[] args) {
         int [] onedim ={1,2,3,4,5};
         System.out.println(onedim[1]);
+        System.out.println(onedim[4]);
     }
 }
