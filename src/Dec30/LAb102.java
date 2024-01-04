cd7c953e0cbdc71d9617d156c8ae9e15237300cf
Lakshmi <meetlaki11@gmail.com>
2024-01-04 23:46:32 +0530

    This is new file .

---
+++ src/Dec30/LAb102.java
@@ -0,0 +1,11 @@
+package src.Dec30;
+
+public class LAb102 {
+    public static void main(String[] args) {
+        int [][] two_Dimensional = {{1,2,3,4,5},{6,7,8,9,10}};
+        System.out.println(two_Dimensional[0][1]);
+        System.out.println(two_Dimensional[1][0]);
+        System.out.println(two_Dimensional[1][4]);
+        System.out.println(two_Dimensional[0][2]);
+    }
+}
