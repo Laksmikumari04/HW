991c01e37cf246a91328a3015c449dd711affad6
Lakshmi <meetlaki11@gmail.com>
2024-01-05 17:11:03 +0530

    This is new file .

---
+++ src/Dec30/Lab119.java
@@ -0,0 +1,11 @@
+package src.Dec30;
+
+public class Lab119 {
+    public static void main(String[] args) {
+        int b=10;
+        int d = --b + b++ +b--;
+        System.out.println(d);
+        System.out.println(b);
+
+    }
+}
