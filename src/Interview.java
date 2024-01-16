327c5f4dd805774cc7887651c1a418f853e3dba9
Lakshmi <meetlaki11@gmail.com>
2024-01-16 12:13:32 +0530

    This is new file .

--- src/Interview.java
+++ src/Interview.java
@@ -3,6 +3,6 @@ package src;
 public class Interview {
     public static void main(String[] args) {
         String name= "shootskill";
-        switch (s)
+        //switch (s)
     }
 }
---
+++ src/Jan6String/SLab01.java
@@ -0,0 +1,14 @@
+package src.Jan6String;
+
+public class SLab01 {
+    public static void main(String[] args) {
+        String s1="Java";// Creating sting by Lieral
+        char ch[] ={'s','t','r','i','n','g','s'};
+        String s2 =new String(ch);// Converting char array to String
+        String s3 = new String("example");// Creating String by new Keyword
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+
+    }
+}
